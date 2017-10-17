05a580aab71964e208e01ea786a30e74f03a65d7
rmoayinla <rmoayinla@yahoo.com>
2017-10-17 10:36:37 +0100

    made the post slide responsive, on mobile devices the slider will scale down to show the slide-body and slide-thumbnail

--- partials/post-slide.php
+++ partials/post-slide.php
@@ -3,11 +3,11 @@ global $post_to_load;
 ?>
 <aside class="archive-post-slide">
 	<div class="row no-inner-margin">
-		<figure class="post-slide-thumbnail col-md-4 no-inner-padding">
+		<figure class="post-slide-thumbnail col-md-4 no-inner-padding col-xs-3">
 			<?php abbey_page_media( "large" ); ?>
 		</figure>
 
-		<div class="post-slide-body col-md-8">
+		<div class="post-slide-body col-md-8 col-xs-9">
 			<h4 class="post-slide-title"> <?php the_title(); ?></h4>
 			<summary class="post-slide-excerpt"> 
 				<?php echo wp_trim_words( get_the_content(), 55, "..." ); ?>
